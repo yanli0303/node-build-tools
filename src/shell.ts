add6bc7833ae9f40676856f05248507998b1c31c
Peter Li <peterleepersonal@gmail.com>
2020-09-17 11:13:34 -0400

    feat(shell): add additional options

--- src/shell.ts
+++ src/shell.ts
@@ -9,18 +9,24 @@ import ps from 'child_process';
  * make commands cross-platform which isn't our goal.
  *
  * @param cmd The command or commands to execute.
- * @param cwd Current working directory, defaults to `process.cwd()`;
+ * @param cwd Current working directory, defaults to `process.cwd()`.
+ * @param options Additional `child_process.ExecSyncOptions`.
  */
-export const shell = (cmd: string | string[], cwd?: string) => {
-  const options: ps.ExecSyncOptions = {
+export const shell = (
+  cmd: string | string[],
+  cwd?: string,
+  options?: ps.ExecSyncOptions,
+) => {
+  const settings: ps.ExecSyncOptions = {
     cwd: cwd || process.cwd(),
     stdio: 'inherit',
     windowsHide: true,
+    ...(options || {}),
   };
 
   const commands = Array.isArray(cmd) ? cmd : [cmd];
   for (const command of commands) {
     console.log(`\n${command}`);
-    ps.execSync(command, options);
+    ps.execSync(command, settings);
   }
 };
